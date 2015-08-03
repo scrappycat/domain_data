e522477c7e35f997720f92afb02d857606747f14
scrappycat <mchayat+scrappycatATgithub@gmail.com>
2015-08-03 15:36:03 +1000

    Initial

--- scraper.py
+++ scraper.py
@@ -4,45 +4,58 @@
 import scraperwiki
 from lxml import html
 from datetime import datetime
+import csv
 
 extractedOn = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M:%S')
 
-# Read in a page
-html_string = scraperwiki.scrape("http://www.domain.com.au/search/buy/state/vic/area/inner-east/region/melbourne-region/suburb/carnegie")
+def parse_page(state="vic", area="inner-east", region="melbourne-region", suburb="carnegie"):
+    # Read in a page
+    html_string = scraperwiki.scrape(
+        "http://www.domain.com.au/search/buy/state/%s/area/%s/region/%s/suburb/%s" % \
+        (state, area, region, suburb)
+    )
 
-# Find something on the page using css selectors
-root = html.fromstring(html_string)
-for elem in root.xpath('//a[@class="link-block"]'):
+    # Find something on the page using css selectors
+    root = html.fromstring(html_string)
+    for elem in root.xpath('//a[@class="link-block"]'):
 
-    item = {
-        "link": "",
-        "address": "",
-        "price": "",
-        "beds": "",
-    }
+        item = {
+            "link": "",
+            "address": "",
+            "price": "",
+            "beds": "",
+        }
 
-    link_elem = elem.xpath("./@href")
-    if len(link_elem)>0:
-        item["link"] = "http://domain.com.au%s" % link_elem[0]
+        link_elem = elem.xpath("./@href")
+        if len(link_elem)>0:
+            item["link"] = "http://domain.com.au%s" % link_elem[0]
 
-    address_elem = elem.xpath('.//h3[contains(@class,"address")=true()]/text()')
-    if len(address_elem)>0:
-        item["address"]=address_elem[0]
+        address_elem = elem.xpath('.//h3[contains(@class,"address")=true()]/text()')
+        if len(address_elem)>0:
+            item["address"]=address_elem[0]
 
-    price_elem = elem.xpath('.//h4[contains(@class,"pricepoint")=true()]/text()')
-    if len(price_elem)>0:
-        item["price"]=price_elem[0]
+        price_elem = elem.xpath('.//h4[contains(@class,"pricepoint")=true()]/text()')
+        if len(price_elem)>0:
+            item["price"]=price_elem[0]
 
-    beds_elem = elem.xpath('.//span[substring(text(),1,3)="Bed"]/../span/em/text()')
-    if len(beds_elem)>0:
-        item["beds"]=beds_elem[0].strip()
+        beds_elem = elem.xpath('.//span[substring(text(),1,3)="Bed"]/../span/em/text()')
+        if len(beds_elem)>0:
+            item["beds"]=beds_elem[0].strip()
 
-# Write out to the sqlite database using scraperwiki library
-scraperwiki.sqlite.save(unique_keys=['address', 'extracted_on'],
-                        data={
-                            "link": item["link"],
-                            "address": item["address"],
-                            "price": item["price"],
-                            "beds": item["beds"],
-                            "extracted_on": extractedOn
-                        })
+        # Write out to the sqlite database using scraperwiki library
+        scraperwiki.sqlite.save(unique_keys=['address', 'extracted_on'],
+                                data={
+                                    "link": item["link"],
+                                    "address": item["address"],
+                                    "price": item["price"],
+                                    "beds": item["beds"],
+                                    "extracted_on": extractedOn
+                                })
+
+
+dictReader = csv.DictReader(open('suburbs.csv', 'rb'))
+
+for line in dictReader:
+    for page in range(1, int(line['pages'])):
+        # Read in a page
+        parse_page(line['suburb'], line['state'], page)
